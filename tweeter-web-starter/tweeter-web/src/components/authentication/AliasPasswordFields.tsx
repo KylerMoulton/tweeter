bfa7beaf979767c0233557ea27057a7bea6a8398
Kyler Moulton <kylermoulton@gmail.com>
2025-02-04 13:22:14 -0700

    First commit

---
+++ tweeter-web-starter/tweeter-web/src/components/authentication/AliasPasswordFields.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+interface Props {
+  alias: string;
+  setAlias: (value: string) => void;
+  password: string;
+  setPassword: (value: string) => void;
+  keyDownFunction: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+}
+
+const AliasPasswordFields = (props: Props) => {
+  return (
+    <>
+      <div className="form-floating">
+        <input
+          type="text"
+          className="form-control"
+          id="aliasInput"
+          placeholder="name@example.com"
+          value={props.alias}
+          onKeyDown={props.keyDownFunction}
+          onChange={(event) => props.setAlias(event.target.value)}
+        />
+        <label htmlFor="aliasInput">Alias</label>
+      </div>
+      <div className="form-floating mb-3">
+        <input
+          type="password"
+          className="form-control bottom"
+          id="passwordInput"
+          placeholder="Password"
+          value={props.password}
+          onKeyDown={props.keyDownFunction}
+          onChange={(event) => props.setPassword(event.target.value)}
+        />
+        <label htmlFor="passwordInput">Password</label>
+      </div>
+    </>
+  );
+};
+
+export default AliasPasswordFields
