51e5dc8b273dee64d0842d9fd5af78f4133a9e8c
Kyler Moulton <kylermoulton@gmail.com>
2025-02-25 11:40:45 -0700

    UserNavigationPresenter Functionality

--- tweeter-web-starter/tweeter-web/src/components/userInfo/UserNavigationHook.ts
+++ tweeter-web-starter/tweeter-web/src/components/userInfo/UserNavigationHook.ts
@@ -1,6 +1,7 @@
-import { AuthToken, FakeData, User } from "tweeter-shared";
+import { UserNavigationPresenter, UserNavigationView } from "../../presenters/UserNavigationPresenter";
 import useToastListener from "../toaster/ToastListenerHook";
 import useUserInfo from "./userInfoHook";
+
 
 interface UseUserNavigation {
   navigateToUser: (event: React.MouseEvent) => Promise<void>;
@@ -10,33 +11,16 @@ const useUserNavigation = (): UseUserNavigation => {
   const { displayErrorMessage } = useToastListener();
   const { setDisplayedUser, currentUser, authToken } =
     useUserInfo();
-    
+
+  const listener: UserNavigationView = {
+    setDisplayedUser,
+    displayErrorMessage
+  }
+
+  const presenter = new UserNavigationPresenter(listener)
+  
   const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
-    event.preventDefault();
-  
-    try {
-      const alias = extractAlias(event.target.toString());
-      const user = await getUser(authToken!, alias);
-  
-      if (user) {
-        setDisplayedUser(currentUser?.equals(user) ? currentUser : user);
-      }
-    } catch (error) {
-      displayErrorMessage(`Failed to get user because of exception: ${error}`);
-    }
-  };
-
-  const extractAlias = (value: string): string => {
-    const index = value.indexOf("@");
-    return value.substring(index);
-  };
-
-  const getUser = async (
-    authToken: AuthToken,
-    alias: string
-  ): Promise<User | null> => {
-    // TODO: Replace with the result of calling the server
-    return FakeData.instance.findUserByAlias(alias);
+    presenter.navigateToUser(event, authToken, currentUser)
   };
 
   return { navigateToUser };
