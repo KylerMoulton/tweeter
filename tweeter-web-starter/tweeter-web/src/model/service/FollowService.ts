d203b3439b1235a5298c4b783b8dd7ad2faf9d2a
Kyler Moulton <kylermoulton@gmail.com>
2025-03-24 18:50:25 -0600

    Followee Lambda

--- tweeter-web-starter/tweeter-web/src/model/service/FollowService.ts
+++ tweeter-web-starter/tweeter-web/src/model/service/FollowService.ts
@@ -1,6 +1,13 @@
-import { AuthToken, User, FakeData } from "tweeter-shared";
+import { AuthToken, User, FakeData, PagedUserItemRequest } from "tweeter-shared";
+import { ServerFacade } from "../../network/ServerFacade";
 
 export class FollowService {
+  private serverFacade: ServerFacade;
+
+  constructor() {
+    this.serverFacade = new ServerFacade();
+  }
+
    public async loadMoreFollowers(
       authToken: AuthToken,
       userAlias: string,
@@ -17,7 +24,12 @@ export class FollowService {
       pageSize: number,
       lastItem: User | null
     ): Promise<[User[], boolean]> {
-      // TODO: Replace with the result of calling server
-      return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
-    };
+      const request: PagedUserItemRequest = {
+        token: authToken.token,
+        userAlias: userAlias,
+        pageSize: pageSize,
+        lastItem: lastItem ? lastItem.dto : null,
+      };
+      return this.serverFacade.getMoreFollowees(request);
+    }
 }
