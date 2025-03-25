ba10a2a8e6eaa5d8a6d48f4162548b92102861a9
Kyler Moulton <kylermoulton@gmail.com>
2025-03-24 20:16:59 -0600

    Follower Lambda

--- tweeter-web-starter/tweeter-web/src/model/service/FollowService.ts
+++ tweeter-web-starter/tweeter-web/src/model/service/FollowService.ts
@@ -9,13 +9,18 @@ export class FollowService {
   }
 
    public async loadMoreFollowers(
-      authToken: AuthToken,
-      userAlias: string,
+    authToken: AuthToken,
+    userAlias: string,
       pageSize: number,
       lastItem: User | null
     ): Promise<[User[], boolean]> {
-      // TODO: Replace with the result of calling server
-      return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
+      const request: PagedUserItemRequest = {
+        token: authToken.token,
+        userAlias: userAlias,
+        pageSize: pageSize,
+        lastItem: lastItem ? lastItem.dto : null,
+      };
+      return this.serverFacade.getMoreFollowees(request);
     };
   
     public async loadMoreFollowees(
